9790d5b1e275fb0314d669f67ad3d0623fae512f
Nathan LeClaire <nathanleclaire@gmail.com>
2012-09-07 00:42:57 -0400

    Got basic scrape of Skyrim Wiki working, and finally figured out what was wrong with my use of jsdom (it didn't enjoy parameters and also getting the latest version of jQuery seems to have helped)

--- skyrimtroll.js
+++ skyrimtroll.js
@@ -1,9 +1,41 @@
 var request = require('request');
+var connect = require('connect');
+var jsdom = require('jsdom');
+var htmlparser = require('htmlparser');
 
-request('http://www.uesp.net/wiki/Skyrim:Guard', function(error, response, body) {
-          if (!error && response.statusCode == 200) {
-            console.log(body);
-          }
-});
+jsdom.env('http://www.uesp.net/wiki/Skyrim:Guard', 
+	[ 'http://code.jquery.com/jquery.min.js' ],
+	function(errors, window) {
+		var $ = window.$;
+		var interval = 500;
+		var timer = 0;
 
+		var $skyrim_quotes = $('tr td').filter( function(element, index, array) {
+				if ( $(index).html().match(/".*"/) ) {
+					return true;
+				} else {
+					return false;
+				}
+		});
 
+		$skyrim_quotes.each( function() {
+			var scopedElem = this;
+			setTimeout(function() {
+				console.log( $(scopedElem).html() );
+			}, timer);
+			timer += interval;
+
+		}); 
+	});
+
+// 'http://www.reddit.com/user/theonewhoquestions.json'
+
+var user = 'theonewhoquestions';
+
+var app = connect()
+	.use(connect.logger('dev'))
+	.use(connect.bodyParser())
+	.use(connect.static('public'))
+	.use(function(req, res) {
+		res.end('hello world\n');
+	}).listen(3000);
